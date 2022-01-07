6b510356291df4819ffb661aecd570ddd2ffe8db
조동윤 <dycho96@gmail.com>
2022-01-07 15:53:01 +0900

    비트마스킹

--- PS/BOJ/1094 막대기.cpp
+++ PS/BOJ/1094 막대기.cpp
@@ -10,9 +10,11 @@ int getSome()
 		{
 			num[j] = 1;
 			N %= (1 << j);
+			/// 비트 삭제
+			/// N ^= (1<<j); // XOR, 토글이기 때문에 원소 추가에도 사용 가능
+			/// N &= ~(1<<j); // 틸트 + AND 연산
 		}
 				
-
 	for (int i = 0; i < 9; i++)
 		if (num[i])
 			ans++;
@@ -20,10 +22,17 @@ int getSome()
 	return ans;
 }
 
+int bitCount(int x)
+{
+	if (x == 0) return 0;
+	return (x % 2) + bitCount(x / 2);
+}
+
 int main()
 {
 	scanf("%d", &N);
 
-	printf("%d\n", getSome());
+	// printf("%d\n", getSome());
+	printf("%d\n", bitCount(N));
 	return 0;
 }
